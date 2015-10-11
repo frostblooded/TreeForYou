ac96894583d9fc15479de9b8d9a7201b8f0969af
Antonio <amindov@abv.bg>
2015-10-11 03:24:12 +0300

    I'm thinking .. to sleep .. or not to sleep

--- javascript/map.js
+++ javascript/map.js
@@ -1,11 +1,15 @@
+class 
+
 var markers = new Array();
 var map;
 var currentMarker = null;
 var initialLocation;
 var browserSupportFlag =  new Boolean();
 var tuesLocation;
-var contentString;
+var addSpaceContent;
 var removeMarker = false;
+var currentTitle = "Title";
+var currentDescription = "You can plant a tree here";
 
 $(document).ready(function(){
 	Parse.initialize("ypv4dSS2h2pN6UTduc8hC9czpjBRJIklN7gN4ULv", "EgGSdtxDzc7GtuIvnGaaF3NBbRmuRRPq6B6yKbRV");
@@ -38,14 +42,18 @@ function initMap() {
 }
 
 function placeMarker(pos, map){	
-	
-	if(currentMarker)
+
+	if(removeMarker)
 	{		
 		currentMarker.setMap(null);
 	}
 	else
 	{
 		removeMarker = true;
+	}
+
+	for (i = 0; i < markers.length; i++) {
+		markers[i].info.close();
 	}
 
 	currentMarker = new google.maps.Marker({
@@ -55,11 +63,15 @@ function placeMarker(pos, map){
 	});
 	
 	currentMarker.info = new google.maps.InfoWindow({
-		content: contentString
+		content: addSpaceContent
 	});
 	
 	currentMarker.addListener("click", function(){
 		this.info.open(map, this);
+		removeMarker = true;
+		for (i = 0; i < markers.length; i++) {
+			markers[i].info.close();
+		}			
 	});
 
 	currentMarker.info.open(map, currentMarker);
@@ -86,7 +98,17 @@ function loadMarkers(){
 						markers[i].info.close();
 					}
 
+					if(removeMarker)
+					{		
+						currentMarker.setMap(null);
+					}
+					else
+					{
+						removeMarker = true;
+					}
+
 					this.info.open(map, this);
+					removeMarker = false;
 				});
 				
 				markers[markers.length-1].setMap(map);
@@ -98,28 +120,66 @@ function loadMarkers(){
 	});
 }
 
+function getFileName()
+{
+	var fullPath = document.getElementById('file-input').value;
+	if (fullPath) {
+		var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+		var filename = fullPath.substring(startIndex);
+		if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+			filename = filename.substring(1);
+		}
+		return filename;
+	}
+}
 
 function saveMarker(){
+
 	localStorage.clear();
 	var ParseSpace = Parse.Object.extend("Spaces");
 	var new_parse_space = new ParseSpace();
 	var space_pos = new Parse.GeoPoint(currentMarker.position.lat(), currentMarker.position.lng());
+	// new_parse_space.set("objectId", $("#infoTitle").html());
 	new_parse_space.set("coordinates", space_pos);
-	new_parse_space.set("info", currentMarker.info.content);
-	new_parse_space.save(null, {
-		success: function(object) {			
-			$("#save_current").text("Saved current marker");			
-			currentMarker.info.close();			
-			markers[markers.length] = currentMarker;
-			currentMarker = 0;			
-			removeMarker = false;
-		},
-		error: function(model, error) {
-		// Show the error message somewhere and let the user try again.
-				$("#save_current").text("Save current spaces");
-				alert("Error: " + error.code + " " + error.message);
-		}
-	});	
+	new_parse_space.set("info", spaceContent);
+
+	// imageUpload
+
+	var fileUploadControl = $("#file-input")[0];
+
+	//get the file from the html element
+	var file = fileUploadControl.files[0];
+
+	if(file)
+	{
+		//name for the file
+		var name = getFileName();
+		//make the file ready for the DB
+		var parseFile = new Parse.File(name, file);
+
+		new_parse_space.set("image", parseFile);
+
+		new_parse_space.save(null, {
+			success: function(object) {			
+				currentMarker.info.close();			
+				markers[markers.length] = currentMarker;
+				currentMarker = 0;
+				removeMarker = false;
+				markers[markers.length-1].info = new google.maps.InfoWindow({
+					content: spaceContent
+				});
+				alert("Success");
+			},
+			error: function(model, error) {
+			// Show the error message somewhere and let the user try again.
+					alert("Error: " + error.code + " " + error.message);
+			}
+		});	
+	}
+	else
+	{
+		alert("You must add an image!");
+	}
 };
 
 function handleNoGeolocation(errorFlag) {
@@ -140,22 +200,44 @@ function readURL(input) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
-            $('.image-label').attr('src', e.target.result);
+            $('#image-label').attr('src', e.target.result);
         }
 
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-contentString ='<div class = "contentsDiv">' +
-					'<h3 class = "infoTitle">Tree_0001</h3><p class = "infoDescription"> You can plant a tree here </p>' +
-					'<div class = "spotPicture"></div>'+
-					'<div class = "infoAddButton">'+
-						'<div class="image-upload"><label for="file-input"><img class="image-label" src="../images/add_picture.png"/></label><input id="file-input" onchange="readURL(this)" type="file"/></div>'+
+function deleteMarker(){
+	currentMarker.setMap(null);
+}
+
+addSpaceContent = '<div id = "contentsDiv">' +
+					'<h3 id = "infoTitle">'+currentTitle+'</h3><p id = "infoDescription">'+currentDescription+'</p>' +
+					'<div id = "spotPicture"></div>'+
+					'<div id = "infoAddButton">'+
+						'<div id="image-upload"><label for="file-input"><img id="image-label" src="../images/add_picture.png"/>'+
+						'</label><input id="file-input" onchange="readURL(this)" type="file"/></div>'+
 					'</div>'+
-					'<div class = "AcceptButton">'+
+					'<div id = "AcceptButton">'+
 						'<button onclick="saveMarker()">Accept</button>'+
 					'</div>'+
+				'</div>';
+
+spaceContent = '<div id = "contentsDiv">' +
+					'<h3 id = "infoTitle">'+currentTitle+'</h3><p id = "infoDescription">'+currentDescription+'</p>' +
+					'<div id = "spotPicture"></div>'+
+					'<div id = "infoAddButton">'+
+						'<div id="image-upload"><label for="file-input"><img id="image-label" src="../images/add_picture.png"/>'+
+						'</label><input id="file-input" onchange="readURL(this)" type="file"/></div>'+
+					'</div>'+
+
+					'<span id = "DeleteButton">'+
+						'<button onclick="deleteMarker()">Delete</button>'+
+					'</span>'+
+
+					'<span id = "UpdateButton">'+
+						'<button onclick="saveMarker()">Update</button>'+
+					'</span>'+
 				'</div>';
 
 /*
@@ -164,18 +246,18 @@ contentString ='<div class = "contentsDiv">' +
     					'<input id="uploadbutton" type="button" value="Upload"> </input>'+
 
 
-contentString = '<div style = "background-color: color: black">'+
-	'<h3 class = "infoTitle">name</h3><p class = "infoDescription"> description </p>' +
+addSpaceContent = '<div style = "background-color: color: black">'+
+	'<h3 id = "infoTitle">name</h3><p id = "infoDescription"> description </p>' +
 
-	'<table style="width:100%"><tr class = "infoTR"><td class = "infoTD"><center>' + 
+	'<table style="width:100%"><tr id = "infoTR"><td id = "infoTD"><center>' + 
 						'<div #id = "spotPicture"></div>'+
 						'</center></td>'+
-		'<td class = "infoTD"><center>'+
+		'<td id = "infoTD"><center>'+
 						'<div #id = "spotPicture"></div>'+
 					'</div></center></td></tr>'+
-		'<tr class = "infoTR"><center><td class = "infoTD"><div class = "infoAddButton">'+
-						'<input type="submit" value = "Add picture" class = "ADDING"/>'+
-					'</div></td><td class = "infoTD">'+
-					'<div class = "infoAddButton">'+
-						'<input type="submit" value = "Add picture" class = "ADDING"/>'+
+		'<tr id = "infoTR"><center><td id = "infoTD"><div id = "infoAddButton">'+
+						'<input type="submit" value = "Add picture" id = "ADDING"/>'+
+					'</div></td><td id = "infoTD">'+
+					'<div id = "infoAddButton">'+
+						'<input type="submit" value = "Add picture" id = "ADDING"/>'+
 					'</div></td></center></tr></table></div>';*/
